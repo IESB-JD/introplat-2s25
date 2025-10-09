9b02c1e07c8f65122313e4018b8e9d42b1fce2cc
Alexandre Chacon <alexandrechacon@outlook.com>
2025-10-09 13:13:10 -0300

    Add triple shot, introduce use of enums and create a new bullet prefab

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
 
+public enum ShotType
+{
+    Single,
+    Double,
+    Triple
+}
+
 public class ShipController : MonoBehaviour
 {
+    public ShotType shotType;
+    
     public float moveSpeed = 5;
     public float rotationSpeed = 200f;
     public Rigidbody2D rb;
@@ -9,17 +18,59 @@ public class ShipController : MonoBehaviour
     private float moveInput = 0;
     private float rotationInput = 0;
     
-    public GameObject bulletPrefab;
+    public GameObject singleShotBulletPrefab;
+    public GameObject doubleShotBulletPrefab;
     public Transform bulletSpawn;
+    
+    public float shotOffSet = 0.5f;
+    
+    public float fireCooldown = 1f;
+    private float _cooldownTimer;
+    
     
     void Update()
     {
         moveInput = Input.GetAxis("Vertical");
         rotationInput = Input.GetAxis("Horizontal");
         
-        if (Input.GetKeyDown(KeyCode.Space))
+        //Check shot cooldown
+        if(_cooldownTimer > 0)
         {
-            Instantiate(bulletPrefab, bulletSpawn.position, transform.rotation);
+            _cooldownTimer -= Time.deltaTime;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space) && _cooldownTimer <= 0)
+        {
+            _cooldownTimer = fireCooldown; // Reset cooldown
+            
+            if(shotType == ShotType.Double)
+            {
+                Instantiate(singleShotBulletPrefab, bulletSpawn.position + transform.right * shotOffSet, transform.rotation);
+                Instantiate(singleShotBulletPrefab, bulletSpawn.position + transform.right * -shotOffSet, transform.rotation);
+            }
+            else if(shotType == ShotType.Triple)
+            {
+                Instantiate(doubleShotBulletPrefab, bulletSpawn.position, transform.rotation);
+                Instantiate(doubleShotBulletPrefab, bulletSpawn.position + transform.right * +shotOffSet, transform.rotation);
+                Instantiate(doubleShotBulletPrefab, bulletSpawn.position + transform.right * -shotOffSet, transform.rotation);
+            }
+            else
+            {
+                Instantiate(singleShotBulletPrefab, bulletSpawn.position, transform.rotation);
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            shotType = ShotType.Single;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            shotType = ShotType.Double;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            shotType = ShotType.Triple;
         }
     }
     
